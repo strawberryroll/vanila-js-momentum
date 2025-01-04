06eef3bfae94923e06c58012bbddeaad8f4d8b00
ahyeon <nuyha234@gmail.com>
2025-01-04 23:02:03 +0900

    feat: weather API

---
+++ js/weather.js
@@ -0,0 +1,19 @@
+const API_KEY = "add6a36d15ef1c39c72dc54ca9fdb2aa";
+
+function onGeoSuccess(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        city.innerText = data.name;
+    });
+}
+
+function onGeoError() {
+    alert("Can't find you. No weather for you.")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
